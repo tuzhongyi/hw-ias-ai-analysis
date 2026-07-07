88faa29ca0e1bbbbc7af023ed913f47aa58dd755
tuzhongyi <tuzhongyi@howell.net.cn>
2026-07-07 09:43:17 +0800

    适配低版本浏览器

--- src/app/common/storage/global.storage.ts
+++ src/app/common/storage/global.storage.ts
@@ -4,7 +4,7 @@ import { EventEmitter, Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class GlobalStorage {
-  version = '1.0.24.2';
+  version = '1.0.24.3';
   unload = new EventEmitter<void>();
 
   display = {
